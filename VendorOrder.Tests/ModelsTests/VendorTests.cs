1e9edc3d05607b34917deee4c105ba7ae28a8411
David Jandron <jandron1211@gmail.com>
2023-03-03 10:43:08 -0800

    Test for vendor list

--- VendorOrder.Tests/ModelsTests/VendorTests.cs
+++ VendorOrder.Tests/ModelsTests/VendorTests.cs
@@ -37,5 +37,16 @@
       Assert.AreEqual(1, result);
     }
 
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "VendorCO 1";
+      string name02 = "VendorCO 2";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    } 
+
  }
 }
