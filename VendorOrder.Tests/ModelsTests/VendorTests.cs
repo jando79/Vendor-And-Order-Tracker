9c1f7f9b3f1a2de12c2cbf5e53f827906277d0be
David Jandron <jandron1211@gmail.com>
2023-03-03 10:23:18 -0800

    ADD test for Vendor constructor

--- VendorOrder.Tests/ModelsTests/VendorTests.cs
+++ VendorOrder.Tests/ModelsTests/VendorTests.cs
@@ -1,19 +1,25 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using ToDoList.Models;
-// using System.Collections.Generic;
-// using System;
+ using Microsoft.VisualStudio.TestTools.UnitTesting;
+ using VendorOrder.Models;
+ using System.Collections.Generic;
+ using System;
 
-// namespace VendorOrder.Tests
-// {
-//   [TestClass]
-//   public class CategoryTests : IDisposable
-//   {
-//     public void Dispose()
-//     {
-//       Category.ClearAll();
+ namespace VendorOrder.Tests
+ {
+   [TestClass]
+   public class VendorTests //: IDisposable
+   {
+     //public void Dispose()
+     //{
+       //Category.ClearAll();
       
-//     }
+    //}
 
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
 
-//   }
-// }
+ }
+}
