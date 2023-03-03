1ab19dc6ae07845b4246fb2efcc3afdcb1ad57e6
David Jandron <jandron1211@gmail.com>
2023-03-03 09:01:43 -0800

    Add formatting to ModelTests and Models to start running first tests

---
+++ VendorOrder/Models/Vendor.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace VendorOrder.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+    
+  }
+}
