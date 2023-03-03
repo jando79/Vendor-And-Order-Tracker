4c4826b4b44b4101d8950b91d0e1e2d8a406c4f3
David Jandron <jandron1211@gmail.com>
2023-03-03 10:06:48 -0800

    tests for get all, and find in OrderTests

--- VendorOrder.Tests/ModelsTests/OrderTests.cs
+++ VendorOrder.Tests/ModelsTests/OrderTests.cs
@@ -6,12 +6,12 @@ using System;
 namespace VendorOrder.Tests
 {
   [TestClass]
-  public class OrderTests //: IDisposable
+  public class OrderTests : IDisposable
   {
-    //public void Dispose()
-    //{
-      //Order.ClearAll();
-    //}
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
 
      [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
@@ -40,8 +40,46 @@ namespace VendorOrder.Tests
       Assert.AreEqual(updatedDescription, result);
     }
 
-  
-  
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newOrder = new List<Order> { };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+     [TestMethod]
+    public void GetAll_ReturnsOrder_OrderList()
+    {
+      string description01 = "bread";
+      string description02 = "pastry";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      List<Order> newOrder = new List<Order> { newOrder1, newOrder2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+     [TestMethod]
+    public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      string description = "bread";
+      Order newOrder = new Order(description);
+      int result = newOrder.Id;
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+    //Arrange
+      string description01 = "bread";
+      string description02 = "pastry";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      Order result = Order.Find(2);
+      Assert.AreEqual(newOrder2, result);
+  }
   
   }
 }
